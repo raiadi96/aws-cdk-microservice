2b7a286bdf0ef11e39207ee5584dd8aaf319207e
Aditya Rai <kaditya1298@gmail.com>
2023-04-23 14:57:54 +0530

    created initial structure for product microservice

--- lib/aws-cdk-microservice-stack.ts
+++ lib/aws-cdk-microservice-stack.ts
@@ -1,16 +1,44 @@
 import * as cdk from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { AssetCode, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import { join } from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkMicroserviceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const productTable = new Table(
+      this, 'ProductTable', {
+        partitionKey: { name: 'id', type: AttributeType.STRING },
+        tableName: 'Product',
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        billingMode: BillingMode.PAY_PER_REQUEST
+      }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkMicroserviceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const nodeJsFunctionProps:NodejsFunctionProps ={
+      bundling: {
+       externalModules: ['aws-sdk']
+    },
+    environment: {
+      PRIMARY_KEY: 'id',
+      TABLE_NAME: productTable.tableName
+    },
+    runtime: Runtime.NODEJS_14_X
+  }
+
+    const ProductFunction = new NodejsFunction(
+      this,
+      'ProductFunction', 
+      {
+        entry:join(__dirname, '../src/product/index.js'),
+        ...nodeJsFunctionProps
+      }
+    );
+    
+    productTable.grantReadWriteData(ProductFunction);
   }
 }
